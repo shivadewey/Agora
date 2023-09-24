0c3d125a5f416c548ee8d1406ee114b7d4c5c4fc
shivadewey <madhire1996@gmail.com>
2023-09-24 14:05:06 -0700

    Initial commit

---
+++ app/src/main/java/in/shiva/agora/MainActivity.kt
@@ -0,0 +1,194 @@
+package `in`.shiva.agora
+
+import `in`.shiva.agora.agora.media.RtcTokenBuilder2
+import `in`.shiva.agora.databinding.ActivityMainBinding
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.SurfaceView
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import io.agora.rtc2.*
+import io.agora.rtc2.video.VideoCanvas
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    private val appId = "5fb926599aeb4ba391c29247cc3b6f71"
+
+    var appCertificate = "b5065fbfa5ed4d8aba0c25de974502b1"
+    var expirationTimeInSeconds = 3600
+    private val channelName = "papayacoders"
+
+    private var token : String? = null
+//    private val token =
+//        "007eJxTYDDp3SWzb5uimNhB/V+mL5lvLmFi/xjmUq06+8V/76bHa+YoMJimJVkamZlaWiamJpkkJRpbGiYbWRqZmCcnGyeZpZkbKnYuTG4IZGRgnyjPxMgAgSA+D0NBYkFiZWJyfkpqUTEDAwAi+yGx"
+
+    private val uid = 0
+    private var isJoined = false
+
+    private var agoraEngine: RtcEngine? = null
+
+    private var localSurfaceView: SurfaceView? = null
+
+    private var remoteSurfaceView: SurfaceView? = null
+
+
+    private val PERMISSION_REQ_ID = 22
+    private val REQUESTED_PERMISSIONS = arrayOf<String>(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.CAMERA
+    )
+
+    private fun checkSelfPermission(): Boolean {
+        return !(ContextCompat.checkSelfPermission(
+            this,
+            REQUESTED_PERMISSIONS[0]
+        ) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(
+                    this,
+                    REQUESTED_PERMISSIONS[1]
+                ) != PackageManager.PERMISSION_GRANTED)
+    }
+
+    fun showMessage(message: String?) {
+        runOnUiThread {
+            Toast.makeText(
+                applicationContext,
+                message,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+
+    private fun setupVideoSDKEngine() {
+        try {
+            val config = RtcEngineConfig()
+            config.mContext = baseContext
+            config.mAppId = appId
+            config.mEventHandler = mRtcEventHandler
+            agoraEngine = RtcEngine.create(config)
+            // By default, the video module is disabled, call enableVideo to enable it.
+            agoraEngine!!.enableVideo()
+        } catch (e: Exception) {
+            showMessage(e.toString())
+        }
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val tokenBuilder = RtcTokenBuilder2()
+        val timestamp = (System.currentTimeMillis() / 1000 + expirationTimeInSeconds).toInt()
+
+        println("UID token")
+        val result = tokenBuilder.buildTokenWithUid(
+            appId, appCertificate,
+            channelName, uid, RtcTokenBuilder2.Role.ROLE_PUBLISHER, timestamp, timestamp
+        )
+        println(result)
+
+
+        token = result
+
+        if (!checkSelfPermission()) {
+            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, PERMISSION_REQ_ID);
+        }
+        setupVideoSDKEngine();
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        agoraEngine!!.stopPreview()
+        agoraEngine!!.leaveChannel()
+
+        Thread {
+            RtcEngine.destroy()
+            agoraEngine = null
+        }.start()
+    }
+
+    private val mRtcEventHandler: IRtcEngineEventHandler = object : IRtcEngineEventHandler() {
+        override fun onUserJoined(uid: Int, elapsed: Int) {
+            showMessage("Remote user joined $uid")
+
+            // Set the remote video view
+            runOnUiThread { setupRemoteVideo(uid) }
+        }
+
+        override fun onJoinChannelSuccess(channel: String, uid: Int, elapsed: Int) {
+            isJoined = true
+            showMessage("Joined Channel $channel")
+        }
+
+        override fun onUserOffline(uid: Int, reason: Int) {
+            showMessage("Remote user offline $uid $reason")
+            runOnUiThread { remoteSurfaceView!!.visibility = View.GONE }
+        }
+    }
+
+    private fun setupRemoteVideo(uid: Int) {
+        remoteSurfaceView = SurfaceView(baseContext)
+        remoteSurfaceView!!.setZOrderMediaOverlay(true)
+        binding.remoteVideoViewContainer.addView(remoteSurfaceView)
+        agoraEngine!!.setupRemoteVideo(
+            VideoCanvas(
+                remoteSurfaceView,
+                VideoCanvas.RENDER_MODE_FIT,
+                uid
+            )
+        )
+        remoteSurfaceView!!.visibility = View.VISIBLE
+    }
+
+    private fun setupLocalVideo() {
+        localSurfaceView = SurfaceView(baseContext)
+        binding.localVideoViewContainer.addView(localSurfaceView)
+        agoraEngine!!.setupLocalVideo(
+            VideoCanvas(
+                localSurfaceView,
+                VideoCanvas.RENDER_MODE_HIDDEN,
+                0
+            )
+        )
+    }
+
+    fun joinChannel(view: View) {
+        if (checkSelfPermission()) {
+            val options = ChannelMediaOptions()
+
+            options.channelProfile = Constants.CHANNEL_PROFILE_COMMUNICATION
+            options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER
+            setupLocalVideo()
+            localSurfaceView!!.visibility = View.VISIBLE
+            agoraEngine!!.startPreview()
+            agoraEngine!!.joinChannel(token, channelName, uid, options)
+        } else {
+            Toast.makeText(applicationContext, "Permissions was not granted", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    fun leaveChannel(view: View) {
+        if (!isJoined) {
+            showMessage("Join a channel first")
+        } else {
+            agoraEngine!!.leaveChannel()
+            showMessage("You left the channel")
+            if (remoteSurfaceView != null) remoteSurfaceView!!.visibility = View.GONE
+            if (localSurfaceView != null) localSurfaceView!!.visibility = View.GONE
+            isJoined = false
+        }
+    }
+
+}
---
+++ app/src/main/java/in/shiva/agora/agora/media/PackableEx.java
@@ -0,0 +1,5 @@
+package in.shiva.agora.agora.media;
+
+public interface PackableEx extends Packable {
+    void unmarshal(ByteBuf in);
+}
